a89509de2e95b6d86599f7d92011e2e31f2c59fe
Sergii Gavrylchenko <sgavr@softserveinc.com>
2023-08-14 16:37:45 +0300

    UC#8 React initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Provider } from "react-redux";
+
+import { store } from "./store";
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Provider store={store}>
+      <div className="App"></div>
+    </Provider>
   );
 }
 
